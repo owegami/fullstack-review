8cd814e694e9f38a1a3da8285e18885dc176b4f1
owegami <busimeiru@gmail.com>
2020-09-14 20:16:23 -0700

    "Add functionality to the mongoDB (schema, model, and save function)"

--- database/index.js
+++ database/index.js
@@ -1,16 +1,41 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+
+})
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  repoId: Number,
+  repoName: String,
+  userName: String,
+  forks: Number,
+  url: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (repoId, repoName, userName, forks, url) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  let newRepoEntry = new Repo({
+    repoId: repoId,
+    repoName: repoName,
+    userName: userName,
+    forks: forks,
+    url: url
+  });
+
+  newRepoEntry.save(function (err, newRepoEntry) {
+    if (err) {
+      return console.error(err);
+    } else {
+      console.log(`Success! We've logged ${newRepoEntry.repoName} into our database!`);
+    }
+  })
 }
 
 module.exports.save = save;
